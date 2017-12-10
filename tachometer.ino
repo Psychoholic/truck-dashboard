5965dd44fd5bb1884611d9b547077a65dc5c52d0
Psychoholic <psychoholic@gmail.com>
2017-12-10 16:43:27 -0500

    adding OLED display

--- tachometer.ino
+++ tachometer.ino
@@ -1,8 +1,23 @@
 #include <Adafruit_NeoPixel.h>
 #include <Math.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define LEDPIN 2 
 #define NUMBER_PIXELS 24 
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMBER_PIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
@@ -16,6 +31,11 @@ int pixelnum=0;
 void setup() {
   strip.begin();
   Serial.begin(19200);
+   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
+     display.setTextSize(4);
+
+     
+ 
 }
 
 void loop() {
@@ -23,6 +43,12 @@ void loop() {
  tach = val * 10;
  pixelnum = tach / 415;
  displayTach(pixelnum);
+
+ display.setCursor(20,20);
+   display.setTextColor(WHITE);
+  display.clearDisplay();
+  display.println(tach);
+  display.display();
 
 }
 
@@ -45,9 +71,6 @@ if(pixelnum > curPos) {
       strip.show();
       --curPos;
 }
-
-
-
   }
 
 
