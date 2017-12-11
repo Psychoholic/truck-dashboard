484bba66a616e7c9ea6bdab7c4052b487801ab38
Psychoholic <psychoholic@gmail.com>
2017-12-11 09:49:52 -0500

    adding logic to only display on OLED every 50th run

--- tachometer.ino
+++ tachometer.ino
@@ -1,7 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #include <Math.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define LEDPIN 2
@@ -10,15 +9,6 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
-
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMBER_PIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 int potPin = 2;
@@ -26,30 +16,32 @@ int val;
 int tach = 0;
 int curPos = 0;
 int pixelnum = 0;
+int x = 1;
 
 
 void setup() {
   strip.begin();
-  Serial.begin(19200);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setTextSize(4);
+  display.setTextColor(WHITE);
+  Serial.begin(19200);
 
-
-
+  
 }
 
 void loop() {
   val = analogRead(potPin);
   tach = val * 10;
   pixelnum = tach / 415; //This is a test value since there are 24 LEDs and the swing from the pot is 10k it gave me a good way to display
-  displayTach(pixelnum);
-
-  display.setCursor(20, 20);
-  display.setTextColor(WHITE);
-  display.clearDisplay();
-  display.println(tach);
-  display.display();
-
+  
+  if ( x < 50 ){
+    displayTach(pixelnum);
+    x++;
+  } else {
+    displayTach(pixelnum);
+    displayOLED(tach);
+    x = 1; 
+  }
 }
 
 void displayTach( int pixelnum) {
@@ -71,6 +63,14 @@ void displayTach( int pixelnum) {
     strip.show();
     --curPos;
   }
+}
+
+void displayOLED ( int tach) {
+  display.clearDisplay();
+  display.setCursor(20, 20);
+  display.println(tach);
+  display.display();
+
 }
 
 
